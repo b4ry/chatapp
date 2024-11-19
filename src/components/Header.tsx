b0651161e12624fff2c2f9e0e95fd10088837052
b4ry <barry90@interia.pl>
2024-11-19 13:30:18 +0100

    - add logout funcionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,12 @@
+import { useAuth } from "../stores/AuthContext";
 import styles from "./Header.module.css";
 
 export default function Header() {
-    return <header className={styles.header}></header>;
+    const { logout } = useAuth();
+
+    return (
+        <header className={styles.header}>
+            <button onClick={ logout } className={styles.logout}>LOGOUT</button>
+        </header>
+    );
 }
