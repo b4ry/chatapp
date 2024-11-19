ceeef22a84487c57f40d2eff678c5b18fc32fef8
b4ry <barry90@interia.pl>
2024-11-19 13:12:56 +0100

    - listen to UserJoinsChat events

--- src/components/UsersList/UsersListSection.tsx
+++ src/components/UsersList/UsersListSection.tsx
@@ -2,8 +2,17 @@ import styles from "./UsersListSection.module.css";
 
 import UsersListHeader from "./UsersListHeader";
 import UsersList from "./UsersList";
+import { useEffect } from "react";
+import { onUserJoinsChat } from "../../services/ChatHubService";
 
 export default function UsersListSection() { 
+    useEffect(() => {
+        onUserJoinsChat((username: string) => {
+            console.log("New user joined the chat:", username);
+        });
+
+    }, []);
+
     return (
         <section className={styles.usersList}>
             <UsersListHeader />
