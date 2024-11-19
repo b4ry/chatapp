f2049e33ddebf139e376f7b2e0b87586f30795fd
b4ry <barry90@interia.pl>
2024-11-19 12:09:18 +0100

    - clean up signalr connection on unmounting

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -2,12 +2,19 @@ import { useEffect } from "react";
 import styles from "./Chat.module.css";
 import ChatWindowSection from "./ChatWindow/ChatWindowSection";
 import UsersListSection from "./UsersList/UsersListSection";
-import { startConnection } from "../services/ChatHubService";
+import { closeConnection, startConnection } from "../services/ChatHubService";
 
 export default function Chat() {
     useEffect(() => {
         const initConnection = async () => await startConnection();
+
         initConnection();
+
+        return () => {
+            const cleanup = async () => await closeConnection();
+            
+            cleanup();
+        };
     }, []);
     
     return (
