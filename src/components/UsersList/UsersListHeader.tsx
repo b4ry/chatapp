62d8c861e3412e13415a26a2548b159b8f6f4f02
b4ry <barry90@interia.pl>
2024-11-20 14:58:10 +0100

    - fix styling for the search box

--- src/components/UsersList/UsersListHeader.tsx
+++ src/components/UsersList/UsersListHeader.tsx
@@ -4,7 +4,7 @@ import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 export default function UsersListHeader({ users, onFilterUsers }: { users: User[], onFilterUsers: React.Dispatch<React.SetStateAction<User[]>>}) {
     function handleSearchTermChange(event: React.ChangeEvent<HTMLInputElement>) {
-        const filteredUsers = users.filter(user => user.username.toLocaleLowerCase().includes(event.target.value));
+        const filteredUsers = users.filter(user => user.username.toLocaleLowerCase().includes(event.target.value.toLocaleLowerCase()));
 
         onFilterUsers(filteredUsers);
     }
