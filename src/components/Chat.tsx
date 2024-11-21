2db233387482cf0b91f65f3baf1dc4aeaf1a5aac
b4ry <barry90@interia.pl>
2024-11-21 15:53:19 +0100

    - add aes encryption by rsa logic, - send the encrypted aes back to the signalr backend

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -2,7 +2,10 @@ import { useEffect } from "react";
 import styles from "./Chat.module.css";
 import ChatWindowSection from "./ChatWindow/ChatWindowSection";
 import UsersListSection from "./UsersList/UsersListSection";
-import { closeConnection, onGetAsymmetricPublicKey, onGetAsymmetricPublicKeyUnsubscribe, startConnection } from "../services/ChatHubService";
+import { closeConnection, invokeStoreSymmetricKey, onGetAsymmetricPublicKey, onGetAsymmetricPublicKeyUnsubscribe, startConnection } from "../services/ChatHubService";
+import { parseRSAKey } from "../services/RSAService";
+import { generateAESKey } from "../services/AESService";
+import forge from "node-forge";
 
 export default function Chat() {
     useEffect(() => {
@@ -10,8 +13,29 @@ export default function Chat() {
 
         initConnection();
 
-        onGetAsymmetricPublicKey((publicKey: string) => {
-            console.log(publicKey);
+        onGetAsymmetricPublicKey(async (publicKey: string) => {
+            const rsa = parseRSAKey(publicKey);
+
+            var { keyBytes: aesKey, iv: aesIV } = await generateAESKey();
+
+            const aesKeyBinary = String.fromCharCode.apply(null, aesKey as unknown as number[]);
+            const aesIVBinary = String.fromCharCode.apply(null, aesIV as unknown as number[]);
+            
+            const encryptedAesKey = rsa.encrypt(aesKeyBinary);
+            const encryptedAesIV = rsa.encrypt(aesIVBinary);
+            
+            // Encode the encrypted data as Base64
+            const encryptedAesKeyBase64 = forge.util.encode64(encryptedAesKey);
+            const encryptedAesIVBase64 = forge.util.encode64(encryptedAesIV);
+            
+            // Store encrypted AES key and IV in localStorage
+            localStorage.setItem("encryptedAesKey", encryptedAesKeyBase64);
+            localStorage.setItem("encryptedAesIV", encryptedAesIVBase64);
+
+            const encryptedAesKeyBytes = Uint8Array.from(encryptedAesKey, c => c.charCodeAt(0));
+            const encryptedAesIVBytes = Uint8Array.from(encryptedAesIV, c => c.charCodeAt(0));
+
+            await invokeStoreSymmetricKey([encryptedAesKeyBytes, encryptedAesIVBytes]);
         });
 
         return () => {
