91ba40f85854ee71bf361ce90e42ac5dbf3388cd
b4ry <barry90@interia.pl>
2024-12-03 12:51:28 +0100

    - style messages

--- src/components/ChatWindow/CurrentChatWindow.tsx
+++ src/components/ChatWindow/CurrentChatWindow.tsx
@@ -1,5 +1,36 @@
 import styles from "./CurrentChatWindow.module.css";
 
+type Message = {
+    sender: string;
+    message: string;
+}
+
 export default function CurrentChatWindow() {
-    return <div className={styles.currentChatWindow}></div>;
+    const messages: Message[] = [ 
+        { sender: "me", message: "Hello there!" },
+        { sender: "you", message: "How are you today?" },
+        { sender: "me", message: "This is a collection of messages." },
+        { sender: "me", message: "Hello there!" },
+        { sender: "me", message: "How are you today?" },
+        { sender: "me", message: "This is a collection of messages." },
+        { sender: "me", message: "Hello there!" },
+        { sender: "you", message: "How are you today?" },
+        { sender: "me", message: "This is a collection of messages." },
+        { sender: "me", message: "Hello there!" },
+        { sender: "you", message: "How are you today?" },
+        { sender: "me", message: "This is a collection of messages." },
+        { sender: "me", message: "Hello there!" },
+        { sender: "me", message: "How are you today?" },
+        { sender: "me", message: "This is a collection of messages." },
+        { sender: "me", message: "Hello there!" },
+        { sender: "you", message: "How are you today?" },
+        { sender: "me", message: "This is a collection of messages." }, ];
+
+    return (
+        <div className={styles.currentChatWindow}>
+            {messages.map(message => 
+                <div className={`${styles.message} ${message.sender === "me" ? styles.userMessage : styles.externalMessage }`}>{message.message}</div>
+            )}
+        </div>
+    );
 }
